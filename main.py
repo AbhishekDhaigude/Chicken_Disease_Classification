fd6507ade5219442c0d0918118ee5f168c5832ad
abhishek dhaigude <abhidhaigude11@gmail.com>
2025-07-14 13:31:52 +0530

    data ingestion error detection

---
+++ main.py
@@ -0,0 +1,4 @@
+# Used in debugging of your code what is wrong will be shown in this log file .
+from cnnClassifier import logger
+
+logger.info("Welcome to my custom log.")
---
+++ src/cnnClassifier/utils/__init__.py
@@ -0,0 +1,2 @@
+# The functions that you will use most frequentlt in project .
+# When you want to use those functions import from here and use it 
